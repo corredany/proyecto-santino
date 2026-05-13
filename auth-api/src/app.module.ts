064571310b9996fa63b0fc59ddb0fb23ed777c55
Arturo <correosdany@outlook.com>
2026-05-13 02:19:37 -0600

    Registra ScheduleModule y LimpiezaService en app.module.ts de auth-api

--- auth-api/src/app.module.ts
+++ auth-api/src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AuthModule } from './auth.module';
 import { UsuariosModule } from './usuarios.module';
+import { LimpiezaService } from './application/services/limpieza.service';
 
 @Module({
   imports: [
+    ScheduleModule.forRoot(),
     ThrottlerModule.forRoot([
       {
         name: 'default',
@@ -27,6 +30,7 @@ import { UsuariosModule } from './usuarios.module';
       provide: APP_GUARD,
       useClass: ThrottlerGuard,
     },
+    LimpiezaService,
   ],
 })
 export class AppModule {}
